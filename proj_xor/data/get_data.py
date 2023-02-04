af3f7c1e9a49161060c232f309d0376df4536689
William Black <w.black@uci.edu>
2023-02-03 20:02:01 -0800

    cleaned up

--- proj_xor/data/get_data.py
+++ proj_xor/data/get_data.py
@@ -1,23 +1,18 @@
 from tensorflow.keras import utils as tf_utils
 from importlib.resources import files
-import proj_xor.data
-import pandas as pd
-import numpy as np
+from pandas import read_csv
+from numpy import uint8, float32
 
 _dataset_path = files("proj_xor.data.datasets")
-_dtype = np.float32
+_dtype = float32
 
 
 def train_data():
-
-    # dataset = get_file(origin=files("proj_xor.data.datasets").joinpath("training_data.txt").as_uri())
-    # print(type(dataset))
-
-    dataset = pd.read_csv(
+    dataset = read_csv(
         files("proj_xor.data.datasets").joinpath("training_data.txt"),
         sep=" ",
         names=["labels", "X1", "X2"],
-        dtype={"labels": np.uint8, "X1": _dtype, "X2": _dtype},
+        dtype={"labels": uint8, "X1": _dtype, "X2": _dtype},
         skipinitialspace=1,
     )
 
@@ -25,15 +20,11 @@ def train_data():
 
 
 def test_data():
-
-    # dataset = get_file(origin=files("proj_xor.data.datasets").joinpath("training_data.txt").as_uri())
-    # print(type(dataset))
-
-    dataset = pd.read_csv(
+    dataset = read_csv(
         files("proj_xor.data.datasets").joinpath("test_data.txt"),
         sep=" ",
         names=["labels", "X1", "X2"],
-        dtype={"labels": np.uint8, "X1": _dtype, "X2": _dtype},
+        dtype={"labels": uint8, "X1": _dtype, "X2": _dtype},
         skipinitialspace=1,
     )
 
