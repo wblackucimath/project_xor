19db20b0925ed1f167888450d46de4b8ca3baafb
William <w.black@uci.edu>
2023-02-04 23:36:24 -0800

    save loss and accuracy of each epoch in a pandas DataFrame

--- proj_xor/__main__.py
+++ proj_xor/__main__.py
@@ -2,17 +2,23 @@ import logging
 from proj_xor.data import get_data
 from proj_xor.models import ProjXORModel
 from tqdm import trange, tqdm
+import pandas as pd
+import tensorflow as tf
 
 
 def main(logging_level=logging.WARNING):
     logging.basicConfig(level=logging_level)
     logging.info("Entering main method.")
 
-    EPOCHS = 500
+    EPOCHS = 5
+
+    metric_df_cols = ["epoch", "run", "val"]
+    loss_df = pd.DataFrame(columns=metric_df_cols)
+    acc_df = pd.DataFrame(columns=metric_df_cols)
 
     model = ProjXORModel()
     for epoch in trange(EPOCHS):
-        
+
         model.train_loss.reset_states()
         model.train_accuracy.reset_states()
         model.test_loss.reset_states()
@@ -24,14 +30,37 @@ def main(logging_level=logging.WARNING):
         for data, labels in get_data.test_data():
             model.test_step(data, labels)
 
-        tqdm.write(
-            f'Epoch {epoch + 1:3.0f}\t'
-            f'Train Loss: {model.train_loss.result():.8f}\t'
-            f'Train Accuracy: {model.train_accuracy.result() * 100:.8f}\t'
-            f'Test Loss: {model.test_loss.result():.8f}\t'
-            f'Test Accuracy: {model.test_accuracy.result() * 100:.8f}'
+        temp_loss_df = pd.DataFrame(
+            [
+                [epoch, "train", float(model.train_loss.result())],
+                [epoch, "test", float(model.test_loss.result())],
+            ],
+            columns=metric_df_cols,
         )
-        
+
+        temp_acc_df = pd.DataFrame(
+            [
+                [epoch, "train", float(model.train_accuracy.result())],
+                [epoch, "test", float(model.test_accuracy.result())],
+            ],
+            columns=metric_df_cols,
+        )
+
+        loss_df = pd.concat([loss_df, temp_loss_df], ignore_index=True)
+        acc_df = pd.concat([acc_df, temp_acc_df], ignore_index=True)
+
+        tqdm.write(
+            f"Epoch {epoch + 1:3.0f}\t"
+            f"Train Loss: {model.train_loss.result():.8f}\t"
+            f"Train Accuracy: {model.train_accuracy.result() * 100:.8f}\t"
+            f"Test Loss: {model.test_loss.result():.8f}\t"
+            f"Test Accuracy: {model.test_accuracy.result() * 100:.8f}"
+        )
+
+    loss_df = loss_df.sort_values(["run", "epoch"], ignore_index=True)
+    acc_df = acc_df.sort_values(["run", "epoch"], ignore_index=True)
+    print(loss_df)
+    print(acc_df)
     logging.info("Exiting main method.")
 
 
