0ba021f8753a7c477da7d6a696aa7cc4fe14db21
William <w.black@uci.edu>
2023-02-04 22:48:58 -0800

    added loss and accuracy monitor for epoch iterations via tqdm.write

--- proj_xor/__main__.py
+++ proj_xor/__main__.py
@@ -1,17 +1,18 @@
 import logging
 from proj_xor.data import get_data
 from proj_xor.models import ProjXORModel
-from tqdm import trange
+from tqdm import trange, tqdm
 
 
 def main(logging_level=logging.WARNING):
     logging.basicConfig(level=logging_level)
     logging.info("Entering main method.")
 
-    EPOCHS = 1000
+    EPOCHS = 500
 
     model = ProjXORModel()
     for epoch in trange(EPOCHS):
+        
         model.train_loss.reset_states()
         model.train_accuracy.reset_states()
         model.test_loss.reset_states()
@@ -23,6 +24,14 @@ def main(logging_level=logging.WARNING):
         for data, labels in get_data.test_data():
             model.test_step(data, labels)
 
+        tqdm.write(
+            f'Epoch {epoch + 1:3.0f}\t'
+            f'Loss: {model.train_loss.result():.5f}\t'
+            f'Accuracy: {model.train_accuracy.result() * 100:.5f}\t'
+            f'Test Loss: {model.test_loss.result():.5f}\t'
+            f'Test Accuracy: {model.test_accuracy.result() * 100:.5f}'
+        )
+        
     logging.info("Exiting main method.")
 
 
