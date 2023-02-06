cdd49a8e1b5899c2d3ad6af865b9205e67c1cc09
William <w.black@uci.edu>
2023-02-05 18:19:51 -0800

    Resfactored to wrap the model

--- proj_xor/__main__.py
+++ proj_xor/__main__.py
@@ -1,8 +1,8 @@
 import logging
 from proj_xor.data import get_data
-from proj_xor.models import ProjXORModel
+from proj_xor.models import ProjXORWrapper
 from proj_xor.plots.gen_plots import plot_loss, plot_accuracy
-from tqdm import trange, tqdm
+
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -12,59 +12,61 @@ def main(logging_level=logging.WARNING):
     logging.basicConfig(level=logging_level)
     logging.info("Entering main method.")
 
-    EPOCHS = 50
+    # EPOCHS = 50
 
-    metric_df_cols = ["epoch", "run", "val"]
-    loss_df = pd.DataFrame(columns=metric_df_cols)
-    acc_df = pd.DataFrame(columns=metric_df_cols)
+    # metric_df_cols = ["epoch", "run", "val"]
+    # loss_df = pd.DataFrame(columns=metric_df_cols)
+    # acc_df = pd.DataFrame(columns=metric_df_cols)
 
-    model = ProjXORModel()
-    for epoch in trange(EPOCHS):
+    # model = ProjXORModel()
+    # for epoch in trange(EPOCHS):
 
-        model.train_loss.reset_states()
-        model.train_accuracy.reset_states()
-        model.test_loss.reset_states()
-        model.test_accuracy.reset_states()
+    #     model.train_loss.reset_states()
+    #     model.train_accuracy.reset_states()
+    #     model.test_loss.reset_states()
+    #     model.test_accuracy.reset_states()
 
-        for data, labels in get_data.train_data():
-            model.train_step(data, labels)
+    #     for data, labels in get_data.train_data():
+    #         model.train_step(data, labels)
 
-        for data, labels in get_data.test_data():
-            model.test_step(data, labels)
+    #     for data, labels in get_data.test_data():
+    #         model.test_step(data, labels)
 
-        temp_loss_df = pd.DataFrame(
-            [
-                [epoch, "train", float(model.train_loss.result())],
-                [epoch, "test", float(model.test_loss.result())],
-            ],
-            columns=metric_df_cols,
-        )
+    #     temp_loss_df = pd.DataFrame(
+    #         [
+    #             [epoch, "train", float(model.train_loss.result())],
+    #             [epoch, "test", float(model.test_loss.result())],
+    #         ],
+    #         columns=metric_df_cols,
+    #     )
 
-        temp_acc_df = pd.DataFrame(
-            [
-                [epoch, "train", float(model.train_accuracy.result())],
-                [epoch, "test", float(model.test_accuracy.result())],
-            ],
-            columns=metric_df_cols,
-        )
+    #     temp_acc_df = pd.DataFrame(
+    #         [
+    #             [epoch, "train", float(model.train_accuracy.result())],
+    #             [epoch, "test", float(model.test_accuracy.result())],
+    #         ],
+    #         columns=metric_df_cols,
+    #     )
 
-        loss_df = pd.concat([loss_df, temp_loss_df], ignore_index=True)
-        acc_df = pd.concat([acc_df, temp_acc_df], ignore_index=True)
+    #     loss_df = pd.concat([loss_df, temp_loss_df], ignore_index=True)
+    #     acc_df = pd.concat([acc_df, temp_acc_df], ignore_index=True)
 
-        tqdm.write(
-            f"Epoch {epoch + 1:3.0f}\t"
-            f"Train Loss: {model.train_loss.result():.8f}\t"
-            f"Train Accuracy: {model.train_accuracy.result() * 100:.8f}\t"
-            f"Test Loss: {model.test_loss.result():.8f}\t"
-            f"Test Accuracy: {model.test_accuracy.result() * 100:.8f}"
-        )
+    #     tqdm.write(
+    #         f"Epoch {epoch + 1:3.0f}\t"
+    #         f"Train Loss: {model.train_loss.result():.8f}\t"
+    #         f"Train Accuracy: {model.train_accuracy.result() * 100:.8f}\t"
+    #         f"Test Loss: {model.test_loss.result():.8f}\t"
+    #         f"Test Accuracy: {model.test_accuracy.result() * 100:.8f}"
+    #     )
 
-    loss_df = loss_df.sort_values(["run", "epoch"], ignore_index=True)
-    acc_df = acc_df.sort_values(["run", "epoch"], ignore_index=True)
-    print(loss_df)
-    print(acc_df)
-    plot_loss(loss_df, save_plt=True, show_plt=False)
-    plot_accuracy(acc_df, save_plt=True, show_plt=False)
+    # loss_df = loss_df.sort_values(["run", "epoch"], ignore_index=True)
+    # acc_df = acc_df.sort_values(["run", "epoch"], ignore_index=True)
+    # print(loss_df)
+    # print(acc_df)
+    # plot_loss(loss_df, save_plt=True, show_plt=False)
+    # plot_accuracy(acc_df, save_plt=True, show_plt=False)
+    M = ProjXORWrapper()
+    M.fit(show_plots=True)
     logging.info("Exiting main method.")
 
 
--- proj_xor/models.py
+++ proj_xor/models.py
@@ -14,71 +14,243 @@ from tensorflow.keras.optimizers.schedules import ExponentialDecay
 
 from tensorflow.math import argmax
 from numpy import uint8
+import pandas as pd
+
+from tqdm import trange, tqdm
+
+from proj_xor.data import get_data
+from proj_xor.plots.gen_plots import plot_loss, plot_accuracy
+
 
 class ProjXORModel(Model):
-    def __init__(self):
+    def __init__(
+        self,
+        loss=None,
+        optimizer=None,
+        optimizer_schedule=None,
+        train_loss_metric=None,
+        test_loss_metric=None,
+        train_acc_metric=None,
+        test_acc_metric=None,
+        layers=None,
+    ):
         super(ProjXORModel, self).__init__()
 
-        self._layers = [
-            Dense(2, activation="relu"),
-            Dense(1, activation="sigmoid"),
-            # Discretization(
-            #     bin_boundaries=[0.5],
-            #     output_mode="int",
-            #     dtype=float
-            # ),     
-        ]
+        if layers is None:
+            self._layers = [
+                Dense(2, activation="relu"),
+                Dense(1, activation="sigmoid"),
+                # Discretization(
+                #     bin_boundaries=[0.5],
+                #     output_mode="int",
+                #     dtype=float
+                # ),
+            ]
+        else:
+            self._layers = layers
 
-        self._loss_dtype = BinaryCrossentropyLoss
-        self.loss_object = self._loss_dtype(from_logits=False)
+        if loss is None:
+            self.loss_object = BinaryCrossentropyLoss(from_logits=False)
+        else:
+            self.loss_object = loss
 
-        self._optimizer_schedule = ExponentialDecay(
-            0.1,
-            decay_steps=10 ** 5,
-            decay_rate=0.96,
-            name="exponential_decay_schedule",
-        )
-        self._optimizer = Nadam(
-            learning_rate=self._optimizer_schedule,
-            name="nadam_optimizer",
-        )
+        if optimizer_schedule is None:
+            self._optimizer_schedule = ExponentialDecay(
+                0.1,
+                decay_steps=10 ** 5,
+                decay_rate=0.96,
+                name="exponential_decay_schedule",
+            )
+        else:
+            self._optimizer_schedule = optimizer_schedule
+        if optimizer is None:
+            self._optimizer = Nadam(
+                learning_rate=self._optimizer_schedule,
+                name="nadam_optimizer",
+            )
+        else:
+            self._optimizer = optimizer
 
-        self._train_test_loss_dtype = BinaryCrossentropyMetric
-        self._train_test_accuracy_dtype = BinaryAccuracy
+        if train_loss_metric is None:
+            self.train_loss = BinaryCrossentropyMetric(name="train_loss")
+        else:
+            self.train_loss = train_loss_metric
+        if train_acc_metric is None:
+            self.train_accuracy = BinaryAccuracy(name="train_accuracy")
+        else:
+            self.train_accuracy = train_acc_metric
 
-        self.train_loss = self._train_test_loss_dtype(name="train_loss")
-        self.train_accuracy = self._train_test_accuracy_dtype(name="train_accuracy")
-
-        self.test_loss = self._train_test_loss_dtype(name="test_loss")
-        self.test_accuracy = self._train_test_accuracy_dtype(name="test_accuracy")
+        if test_loss_metric is None:
+            self.test_loss = BinaryCrossentropyMetric(name="test_loss")
+        else:
+            self.test_loss = test_loss_metric
+        if test_acc_metric is None:
+            self.test_accuracy = BinaryAccuracy(name="test_accuracy")
+        else:
+            self.test_accuracy = test_acc_metric
 
     def call(self, x):
         for f in self._layers:
             x = f(x)
         return x
 
+
+class ProjXORWrapper:
+    def __init__(
+        self,
+        loss=None,
+        optimizer=None,
+        optimizer_schedule=None,
+        train_loss_metric=None,
+        test_loss_metric=None,
+        train_acc_metric=None,
+        test_acc_metric=None,
+        layers=None,
+        epochs=50,
+    ):
+        self._is_fitted = False
+
+        self._epochs = epochs
+
+        if layers is None:
+            self._layers = [
+                Dense(2, activation="relu"),
+                Dense(1, activation="sigmoid"),
+                # Discretization(
+                #     bin_boundaries=[0.5],
+                #     output_mode="int",
+                #     dtype=float
+                # ),
+            ]
+        else:
+            self._layers = layers
+
+        if loss is None:
+            self.loss_object = BinaryCrossentropyLoss(from_logits=False)
+        else:
+            self.loss_object = loss
+
+        if optimizer_schedule is None:
+            self._optimizer_schedule = ExponentialDecay(
+                0.1,
+                decay_steps=10 ** 5,
+                decay_rate=0.96,
+                name="exponential_decay_schedule",
+            )
+        else:
+            self._optimizer_schedule = optimizer_schedule
+        if optimizer is None:
+            self._optimizer = Nadam(
+                learning_rate=self._optimizer_schedule,
+                name="nadam_optimizer",
+            )
+        else:
+            self._optimizer = optimizer
+
+        if train_loss_metric is None:
+            self.train_loss = BinaryCrossentropyMetric(name="train_loss")
+        else:
+            self.train_loss = train_loss_metric
+        if train_acc_metric is None:
+            self.train_accuracy = BinaryAccuracy(name="train_accuracy")
+        else:
+            self.train_accuracy = train_acc_metric
+
+        if test_loss_metric is None:
+            self.test_loss = BinaryCrossentropyMetric(name="test_loss")
+        else:
+            self.test_loss = test_loss_metric
+        if test_acc_metric is None:
+            self.test_accuracy = BinaryAccuracy(name="test_accuracy")
+        else:
+            self.test_accuracy = test_acc_metric
+
+        self._model = ProjXORModel(
+            loss=self.loss_object,
+            optimizer=self._optimizer,
+            optimizer_schedule=self._optimizer_schedule,
+            train_loss_metric=self.train_loss,
+            test_loss_metric=self.test_loss,
+            train_acc_metric=self.train_accuracy,
+            test_acc_metric=self.test_accuracy,
+            layers=self._layers,
+        )
+
+        self.train_ds = get_data.train_data()
+        self.test_ds = get_data.test_data()
+
     @tf.function
-    def train_step(self, data, labels, ret_loss=False):
+    def train_step(self, data, labels):
         with tf.GradientTape() as tape:
-            predictions = self(data, training=True)
+            predictions = self._model(data, training=True)
             print(predictions, labels)
             loss = self.loss_object(labels, predictions)
-        gradients = tape.gradient(loss, self.trainable_variables)
-        self._optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        gradients = tape.gradient(loss, self._model.trainable_variables)
+        self._optimizer.apply_gradients(zip(gradients, self._model.trainable_variables))
 
         los = self.train_loss(labels, predictions)
         acc = self.train_accuracy(labels, predictions)
 
-        if ret_loss:
-            return {"loss": los, "accuracy": acc}
-
     @tf.function
-    def test_step(self, data, labels, ret_loss=False):
-        predictions = self(data, training=False)
+    def test_step(self, data, labels):
+        predictions = self._model(data, training=False)
         t_loss = self.loss_object(labels, predictions)
 
         los = self.test_loss(labels, predictions)
         acc = self.test_accuracy(labels, predictions)
 
-        if ret_loss:
-            return {"loss": los, "accuracy": acc}
+    def fit(self, save_dfs=False, save_plots=False, show_plots=False):
+        EPOCHS = self._epochs
+
+        metric_df_cols = ["epoch", "run", "val"]
+        loss_df = pd.DataFrame(columns=metric_df_cols)
+        acc_df = pd.DataFrame(columns=metric_df_cols)
+
+        for epoch in trange(EPOCHS):
+
+            self.train_loss.reset_states()
+            self.train_accuracy.reset_states()
+            self.test_loss.reset_states()
+            self.test_accuracy.reset_states()
+
+            for data, labels in self.train_ds:
+                self.train_step(data, labels)
+
+            for data, labels in self.test_ds:
+                self.test_step(data, labels)
+
+            temp_loss_df = pd.DataFrame(
+                [
+                    [epoch, "train", float(self.train_loss.result())],
+                    [epoch, "test", float(self.test_loss.result())],
+                ],
+                columns=metric_df_cols,
+            )
+
+            temp_acc_df = pd.DataFrame(
+                [
+                    [epoch, "train", float(self.train_accuracy.result())],
+                    [epoch, "test", float(self.test_accuracy.result())],
+                ],
+                columns=metric_df_cols,
+            )
+
+            loss_df = pd.concat([loss_df, temp_loss_df], ignore_index=True)
+            acc_df = pd.concat([acc_df, temp_acc_df], ignore_index=True)
+
+            tqdm.write(
+                f"Epoch {epoch + 1:3.0f}\t"
+                f"Train Loss: {self.train_loss.result():.8f}\t"
+                f"Train Accuracy: {self.train_accuracy.result() * 100:.8f}\t"
+                f"Test Loss: {self.test_loss.result():.8f}\t"
+                f"Test Accuracy: {self.test_accuracy.result() * 100:.8f}"
+            )
+
+        loss_df = loss_df.sort_values(["run", "epoch"], ignore_index=True)
+        acc_df = acc_df.sort_values(["run", "epoch"], ignore_index=True)
+        print(loss_df)
+        print(acc_df)
+        plot_loss(loss_df, save_plt=save_plots, show_plt=show_plots)
+        plot_accuracy(acc_df, save_plt=save_plots, show_plt=show_plots)
+
+        self._is_fitted = True
